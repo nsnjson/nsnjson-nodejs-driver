e164cebb4963e6af52694bb591ded34b9ce34c1d
Maks Atygaev <jxcoder@ya.ru>
2015-10-28 17:42:20 +0600

    Add function "decode" to driver interface

--- src/nsnjson.driver.js
+++ src/nsnjson.driver.js
@@ -1,5 +1,6 @@
 module.exports = {
   encoder: require('./nsnjson.encoder'),
   decoder: require('./nsnjson.decoder'),
-  encode: require('./nsnjson.encoder').encode
+  encode: require('./nsnjson.encoder').encode,
+  decode: require('./nsnjson.decoder').decode
 };
