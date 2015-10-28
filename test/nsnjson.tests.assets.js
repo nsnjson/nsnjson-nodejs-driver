e2d214de1a7595d52467dde129bf136a5583c507
Maks Atygaev <jxcoder@ya.ru>
2015-10-28 19:43:04 +0600

    Combine assets [ value6.json, value6presentation.json ] to asset6.json

--- test/nsnjson.tests.assets.js
+++ test/nsnjson.tests.assets.js
@@ -7,7 +7,7 @@ var values = [];
 var presentations = [];
 
 for (var i = 1; i <= assetsCount; i++) {
-  if (i > 5) {
+  if (i > 6) {
     values.push(require(path + 'value' + i + '.json'));
 
     presentations.push(require(path + 'value' + i + 'presentation.json'));
