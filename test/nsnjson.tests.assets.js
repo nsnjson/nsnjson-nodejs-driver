d0cf1451ed91b69717d58bfae1d56d425dfb189b
Maks Atygaev <jxcoder@ya.ru>
2015-10-28 18:42:48 +0600

    Combine assets [ value1.json, value1presentation.json ] to asset1.json

--- test/nsnjson.tests.assets.js
+++ test/nsnjson.tests.assets.js
@@ -7,9 +7,17 @@ var values = [];
 var presentations = [];
 
 for (var i = 1; i <= assetsCount; i++) {
-  values.push(require(path + 'value' + i + '.json'));
+  if (i > 1) {
+    values.push(require(path + 'value' + i + '.json'));
 
-  presentations.push(require(path + 'value' + i + 'presentation.json'));
+    presentations.push(require(path + 'value' + i + 'presentation.json'));
+  } else {
+    var asset = require(path + 'asset' + i + '.json');
+
+    values.push(asset.data);
+
+    presentations.push(asset.presentation);
+  }
 }
 
 module.exports = {
