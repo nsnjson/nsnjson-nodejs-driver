7ad7ff67bdef1d30c2233cadc05e7e779eff370c
Maks Atygaev <jxcoder@ya.ru>
2015-10-30 10:17:26 +0700

    Refactor { nsnjson.decoder @ decode } : implement decoding context

--- src/nsnjson.decoder.js
+++ src/nsnjson.decoder.js
@@ -2,11 +2,11 @@ var Maybe = require('data.maybe');
 
 var Format = require('./nsnjson.format');
 
-function decodeNull(presentation) {
+function decodeNull(context) {
   return Maybe.Just(null);
 }
 
-function decodeBoolean(presentation) {
+function decodeBoolean(context, presentation) {
   if (presentation.v == 1) {
     return Maybe.Just(true);
   }
@@ -18,15 +18,15 @@ function decodeBoolean(presentation) {
   return Maybe.Nothing();
 };
 
-function decodeNumber(presentation) {
+function decodeNumber(context, presentation) {
   return Maybe.Just(presentation.v);
 };
 
-function decodeString(presentation) {
+function decodeString(context, presentation) {
   return Maybe.Just(presentation.v);
 };
 
-function decodeArray(presentation) {
+function decodeArray(context, presentation) {
   var array = [];
 
   var encodedItems = presentation.v;
@@ -34,7 +34,7 @@ function decodeArray(presentation) {
   for (var i = 0, size = encodedItems.length; i < size; i++) {
     var encodedItem = encodedItems[i];
 
-    var itemMaybe = decode(encodedItem);
+    var itemMaybe = decode(context, encodedItem);
 
     if (itemMaybe.isJust) {
       var item = itemMaybe.get();
@@ -46,7 +46,7 @@ function decodeArray(presentation) {
   return Maybe.Just(array);
 };
 
-function decodeObject(presentation) {
+function decodeObject(context, presentation) {
   var object = {};
 
   var encodedFields = presentation.v;
@@ -54,7 +54,7 @@ function decodeObject(presentation) {
   for (var i = 0, size = encodedFields.length; i < size; i++) {
     var encodedField = encodedFields[i];
 
-    var fieldValueMaybe = decode(encodedField);
+    var fieldValueMaybe = decode(context, encodedField);
 
     if (fieldValueMaybe.isJust) {
       var value = fieldValueMaybe.get();
@@ -68,46 +68,15 @@ function decodeObject(presentation) {
   return Maybe.Just(object);
 };
 
-function checkerByType(type) {
-  return function(presentation) {
-    return presentation.t == type;
-  };
-};
+function decode(context, presentation) {
+  var resolvers = context.resolvers;
 
-var resolvers = {
-  'null': {
-    checker: checkerByType(Format.TYPE_MARKER_NULL),
-    decoder: decodeNull
-  },
-  'number': {
-    checker: checkerByType(Format.TYPE_MARKER_NUMBER),
-    decoder: decodeNumber
-  },
-  'string': {
-    checker: checkerByType(Format.TYPE_MARKER_STRING),
-    decoder: decodeString
-  },
-  'boolean': {
-    checker: checkerByType(Format.TYPE_MARKER_BOOLEAN),
-    decoder: decodeBoolean
-  },
-  'array': {
-    checker: checkerByType(Format.TYPE_MARKER_ARRAY),
-    decoder: decodeArray
-  },
-  'object': {
-    checker: checkerByType(Format.TYPE_MARKER_OBJECT),
-    decoder: decodeObject
-  }
-};
-
-function decode(presentation) {
   for (var resolverName in resolvers) {
     if (resolvers.hasOwnProperty(resolverName)) {
       var resolver = resolvers[resolverName];
 
       if (resolver.checker(presentation)) {
-        return resolver.decoder(presentation);
+        return resolver.decoder(context, presentation);
       }
     }
   }
@@ -117,6 +86,41 @@ function decode(presentation) {
 
 module.exports = {
   decode: function(presentation) {
-    return decode(presentation);
+    function checkerByType(type) {
+      return function(presentation) {
+        return presentation.t == type;
+      };
+    }
+
+    var context = {
+      resolvers: {
+        'null': {
+          checker: checkerByType(Format.TYPE_MARKER_NULL),
+          decoder: decodeNull
+        },
+        'number': {
+          checker: checkerByType(Format.TYPE_MARKER_NUMBER),
+          decoder: decodeNumber
+        },
+        'string': {
+          checker: checkerByType(Format.TYPE_MARKER_STRING),
+          decoder: decodeString
+        },
+        'boolean': {
+          checker: checkerByType(Format.TYPE_MARKER_BOOLEAN),
+          decoder: decodeBoolean
+        },
+        'array': {
+          checker: checkerByType(Format.TYPE_MARKER_ARRAY),
+          decoder: decodeArray
+        },
+        'object': {
+          checker: checkerByType(Format.TYPE_MARKER_OBJECT),
+          decoder: decodeObject
+        }
+      }
+    };
+
+    return decode(context, presentation);
   }
 };
