721a39a4b02c69ca5ddb5fa77f33600c63651cae
Maks Atygaev <jxcoder@ya.ru>
2015-11-02 06:35:37 +0700

    Add important documentation to { nsnjson.driver }

--- src/nsnjson.driver.js
+++ src/nsnjson.driver.js
@@ -1,4 +1,23 @@
+var Encoder = require('./nsnjson.encoder');
+
+var Decoder = require('./nsnjson.decoder');
+/**
+ * @module Driver
+ */
 module.exports = {
-  encode: require('./nsnjson.encoder').encode,
-  decode: require('./nsnjson.decoder').decode
+  /**
+   * @param {JSON} data any JSON data
+   * @param {Object} options Encoder options
+   */
+  encode: function(data, options) {
+    return Encoder.encode(data, options);
+  },
+
+  /**
+   * @param {JSON} presentation NSNJSON presentation of JSON
+   * @param {Object} options Decoder options
+   */
+  decode: function(presentation, options) {
+    return Decoder.decode(presentation, options);
+  }
 };
