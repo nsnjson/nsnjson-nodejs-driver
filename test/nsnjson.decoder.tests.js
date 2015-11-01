335edfa3dc3087c831ee1831a5e8fd9c119fa4c1
Maks Atygaev <jxcoder@ya.ru>
2015-11-02 04:43:27 +0700

    Refactor { nsnjson.decoder.tests }

--- test/nsnjson.decoder.tests.js
+++ test/nsnjson.decoder.tests.js
@@ -5,21 +5,19 @@ var Decoder = require('../src/nsnjson.decoder');
 var Assets = require('./nsnjson.tests.assets');
 
 describe('Decoder @ decode', function() {
-  function testDecoding(presentation, value) {
-    it(JSON.stringify(value), function() {
-      var actualValueMaybe = Decoder.decode(presentation);
+  function testDecoding(name, data, presentation) {
+    it(name, function() {
+      var actualDataMaybe = Decoder.decode(presentation);
 
-      assert.equal(actualValueMaybe.isJust, true);
+      assert.equal(actualDataMaybe.isJust, true);
 
-      var actualValue = actualValueMaybe.get();
-
-      assert.deepEqual(value, actualValue);
+      assert.deepEqual(data, actualDataMaybe.get());
     });
   };
 
   for (var i = 0; i < Assets.size; i++) {
     var asset = Assets.assets[i];
 
-    testDecoding(asset.presentation, asset.data);
+    testDecoding(asset.name, asset.data, asset.presentation);
   }
 });
