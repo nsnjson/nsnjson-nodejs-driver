df4cade48fd9c77877ce309bac2db20fa9296827
Maks Atygaev <jxcoder@ya.ru>
2015-11-07 22:30:07 +0600

    Implement Encoding in { nsnjson.encoding }

---
+++ src/nsnjson.encoding.js
@@ -0,0 +1,79 @@
+function Encoding() {}
+
+Encoding.isNull = function(data) {
+  return (data == null);
+}
+
+Encoding.isBoolean = function(data) {
+  return (typeof(data) == 'boolean') || (data instanceof Boolean);
+}
+
+Encoding.isNumber = function(data) {
+  return (typeof(data) == 'number') || (data instanceof Number);
+}
+
+Encoding.isString = function(data) {
+  return (typeof(data) == 'string') || (data instanceof String);
+}
+
+Encoding.isArray = function(data) {
+  return (data instanceof Array);
+}
+
+Encoding.isObject = function(data) {
+  return (data instanceof Object);
+}
+
+Encoding.prototype.encodeNull = function() {
+  throw new Error('Method { Encoding @ encodeNull } is not implemented!');
+}
+
+Encoding.prototype.encodeNumber = function(value) {
+  throw new Error('Method { Encoding @ encodeNumber } is not implemented!');
+}
+
+Encoding.prototype.encodeString = function(value) {
+  throw new Error('Method { Encoding @ encodeString } is not implemented!');
+}
+
+Encoding.prototype.encodeBoolean = function(value) {
+  throw new Error('Method { Encoding @ encodeBoolean } is not implemented!');
+}
+
+Encoding.prototype.encodeArray = function(array) {
+  throw new Error('Method { Encoding @ encodeArray } is not implemented!');
+}
+
+Encoding.prototype.encodeObject = function(object) {
+  throw new Error('Method { Encoding @ encodeObject } is not implemented!');
+}
+
+Encoding.prototype.encode = function(data) {
+  if (Encoding.isNull(data)) {
+    return this.encodeNull();
+  }
+
+  if (Encoding.isNumber(data)) {
+    return this.encodeNumber(data);
+  }
+
+  if (Encoding.isString(data)) {
+    return this.encodeString(data);
+  }
+
+  if (Encoding.isBoolean(data)) {
+    return this.encodeBoolean(data);
+  }
+
+  if (Encoding.isArray(data)) {
+    return this.encodeArray(data);
+  }
+
+  if (Encoding.isObject(data)) {
+    return this.encodeObject(data);
+  }
+
+  return Maybe.Nothing();
+}
+
+module.exports = Encoding;
