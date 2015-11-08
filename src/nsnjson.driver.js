3eb68f9696f860f1fbb2e03380754dd3636ce465
Maks Atygaev <jxcoder@ya.ru>
2015-11-08 06:22:08 +0600

    Refactor Driver

--- src/nsnjson.driver.js
+++ src/nsnjson.driver.js
@@ -1,3 +1,5 @@
+var Maybe = require('data.maybe');
+
 var Types = require('./nsnjson.types');
 
 var Format = require('./nsnjson.format');
@@ -15,43 +17,67 @@ var ArrayStyleDecoding = require('./nsnjson.decoding.style.array');
 var ObjectStyleEncoding = require('./nsnjson.encoding.style.object');
 var ObjectStyleDecoding = require('./nsnjson.decoding.style.object');
 
+function Driver() {}
+
+Driver.encode = function(data, customEncoders) {
+  return Encoder.encode(data, customEncoders);
+}
+
+Driver.decode = function(data, customDecoders) {
+  return Decoder.decode(data, customDecoders);
+}
+
+Driver.encoderWithArrayStyle = function(customEncoders) {
+  return Encoding.customize(ArrayStyleEncoding, customEncoders);
+}
+
+Driver.encoderWithObjectStyle = function(customEncoders) {
+  return Encoding.customize(ObjectStyleEncoding, customEncoders);
+}
+
+Driver.decoderWithArrayStyle = function(customDecoders) {
+  return Decoding.customize(ArrayStyleDecoding, customDecoders);
+}
+
+Driver.decoderWithObjectStyle = function(customDecoders) {
+  return Decoding.customize(ObjectStyleDecoding, customDecoders);
+}
+
+Driver.withArrayStyle = function(customizations) {
+  var customEncoders = Maybe.fromNullable(customizations).chain(function($) { return Maybe.fromNullable($.encoders); }).getOrElse({});
+  var customDecoders = Maybe.fromNullable(customizations).chain(function($) { return Maybe.fromNullable($.decoders); }).getOrElse({});
+
+  var encoder = Driver.encoderWithArrayStyle(customEncoders);
+  var decoder = Driver.decoderWithArrayStyle(customDecoders);
+
+  return {
+    encode: function(data) {
+      return encoder.encode(data);
+    },
+    decode: function(presentation) {
+      return decoder.decode(presentation);
+    }
+  };
+}
+
+Driver.withObjectStyle = function(customizations) {
+  var customEncoders = Maybe.fromNullable(customizations).chain(function($) { return Maybe.fromNullable($.encoders); }).getOrElse({});
+  var customDecoders = Maybe.fromNullable(customizations).chain(function($) { return Maybe.fromNullable($.decoders); }).getOrElse({});
+
+  var encoder = Driver.encoderWithObjectStyle(customEncoders);
+  var decoder = Driver.decoderWithObjectStyle(customDecoders);
+
+  return {
+    encode: function(data) {
+      return encoder.encode(data);
+    },
+    decode: function(presentation) {
+      return decoder.decode(presentation);
+    }
+  };
+}
+
 /**
  * @module Driver
  */
-module.exports = {
-  Types: Types,
-
-  Format: Format,
-
-  /**
-   * @param {JSON} data any JSON data
-   * @param {Object} options Encoder options
-   */
-  encode: function(data, options) {
-    return Encoder.encode(data, options);
-  },
-
-  /**
-   * @param {JSON} presentation NSNJSON presentation of JSON
-   * @param {Object} options Decoder options
-   */
-  decode: function(presentation, options) {
-    return Decoder.decode(presentation, options);
-  },
-
-  encoderWithArrayStyle: function(customEncoders) {
-    return Encoding.customize(ArrayStyleEncoding, customEncoders);
-  },
-
-  decoderWithArrayStyle: function(customDecoders) {
-    return Decoding.customize(ArrayStyleDecoding, customDecoders);
-  },
-
-  encoderWithObjectStyle: function(customEncoders) {
-    return Encoding.customize(ObjectStyleEncoding, customEncoders);
-  },
-
-  decoderWithObjectStyle: function(customDecoders) {
-    return Decoding.customize(ObjectStyleDecoding, customDecoders);
-  }
-};
+module.exports = Driver;
